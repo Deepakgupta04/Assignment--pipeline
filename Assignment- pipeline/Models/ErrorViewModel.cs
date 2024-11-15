81de3b11a23294d8cc80c948a0b20219b70417f0
deepak <deepak@gmail.com>
2024-11-15 09:53:24 +0530

    Add project files.

---
+++ Assignment- pipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment__pipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
